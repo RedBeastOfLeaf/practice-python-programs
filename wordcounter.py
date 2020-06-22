35d6f3bed12afb59d000869881cd604ee1169018
VIVEK TIWARI <63796877+vivekuchiha12@users.noreply.github.com>
2020-06-22 10:23:11 +0530

    comments

--- wordcounter.py
+++ wordcounter.py
@@ -1,3 +1,7 @@
+# To count the words in a file
+# The file should be in the same folder or directory
+# enter the file name with extension
+
 fname = input("Enter file name: ")
 num_words = 0
 with open(fname, 'r') as f:
