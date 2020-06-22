f0b5fc43d9545958d6494eb141ff8e601a9ab121
VIVEK TIWARI <63796877+vivekuchiha12@users.noreply.github.com>
2020-06-22 10:15:06 +0530

    the .upper() function was added for more accuracy of the program

--- anagram.py
+++ anagram.py
@@ -1,8 +1,10 @@
 from collections import Counter
 print("")
-first_string = input("Enter the main String: ".upper())
-second_string = input("Enter the String to be checked: ".upper())
-if(Counter(first_string) == Counter(second_string)):
+first_string = input("Enter the main String: ")
+second_string = input("Enter the String to be checked: ")
+string_1 = first_string.upper()
+string_2 = second_string.upper()
+if(Counter(string_1) == Counter(string_2)):
     print("\nThis is an Anagram")
 else:
     print("\nNot An Anagram")    
