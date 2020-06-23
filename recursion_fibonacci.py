430689e593eaa0abcb0bbe946144a90f10c25343
VIVEK TIWARI <63796877+vivekuchiha12@users.noreply.github.com>
2020-06-23 17:20:41 +0530

    Add files via upload

---
+++ recursion_fibonacci.py
@@ -0,0 +1,15 @@
+def recur_fibonacci(n):
+    """Recursive function to print fibonnaci series"""
+    if n <= 1:
+        return n
+    else:
+        return (recur_fibonacci(n-1) + recur_fibonacci(n-2))    
+
+terms = int(input("\nHow many terms do you want? :  "))
+if terms <= 0:
+    print("\nPlease enter a positive integer number.")
+else:
+    print("\nFibonnaci series: ")
+    for i in range(terms):
+        print(recur_fibonacci(i))
+print("\n######    END    ######")
